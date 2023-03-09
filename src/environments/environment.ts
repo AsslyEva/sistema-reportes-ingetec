25ec164c0535dd6f2e4361faad56816e8e180200
Bryantonelly <bryan.antonelly.villegas.cuba@gmail.com>
2023-03-09 15:36:20 -0500

    Update/ actualizacion tabla reporte actividades

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   urlEndPoint: "http://localhost:5000/backend/",
+  // urlEndPoint: "https://ingetecsistemareportes.site/backend/",
   systemName: 'SISTEMA DE GESTION DE ACTIVIDADES COMERCIALES',
   production: true,
   version: "v1.0.0"
