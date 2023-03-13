68a8cd35a1f1c628a2ea65dd7b17aa828e1fd324
Bryantonelly <bryan.antonelly.villegas.cuba@gmail.com>
2023-03-13 18:42:59 -0500

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  urlEndPoint: "http://localhost:5000/backend/",
-  // urlEndPoint: "https://ingetecsistemareportes.site/backend/",
+  // urlEndPoint: "http://localhost:5000/backend/",
+  urlEndPoint: "https://ingetecsistemareportes.site/backend/",
   systemName: 'SISTEMA DE GESTION DE ACTIVIDADES COMERCIALES',
   production: true,
   version: 'v1.0.0'
